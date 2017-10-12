8f223322c128f21faffeefe35c2aed164d668f52
MauroGentile <mauro.gentile@iese.net>
2017-10-12 11:47:56 +0200

    Done

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,22 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(open("football.csv"), "html.parser")
+
+table = soup.find('table', {'class': 'js-csv-data csv-data js-file-line-container'})
+data = [[td.text for td in tr.find_all('td')][1:] 
+    for tr in table.find_all('tr', {'class': "js-file-line"})][1:]
+
+team=data[0][0]
+diff=int(data[0][-3])-int(data[0][-2])
+
+for t in data[1:]:
+    if int(t[-3])-int(t[-2])<diff:
+        diff=int(t[-3])-int(t[-2])
+        team=t[0]
+        
+print(team + " is the team with lowest difference between Goals done and Goals Allowed")
+print("The goals difference is "+ str(diff))
+        
