97a7cc22d2d4e7ce0e4f27b93f914452281d6b6c
MauroGentile <mauro.gentile@iese.net>
2017-10-16 20:37:14 +0200

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,17 @@
-PLACE YOUR CODE HERE
+from bs4 import BeautifulSoup
+import csv
+import re
+
+from advanced_python_regex import read_data, email_addresses_list
+
+
+
+
+data=read_data("faculty.csv")
+emails=email_addresses_list(data)
+
+
+with open("emails.csv", "w", newline='') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    for line in emails:
+        writer.writerow([line])
