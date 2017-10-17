b12081c4add820f438a5961f619d8baa13b01431
MauroGentile <mauro.gentile@iese.net>
2017-10-17 09:59:59 +0200

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,5 +1,5 @@
 #Q6
-
+print("\nQ6")
 faculty_dict = { 'Ellenberg': [['Ph.D.', 'Professor', 'sellenbe@upenn.edu'], ['Ph.D.', 'Professor', 'jellenbe@mail.med.upenn.edu']],
               'Li': [['Ph.D.', 'Assistant Professor', 'liy3@email.chop.edu'], ['Ph.D.', 'Associate Professor', 'mingyao@mail.med.upenn.edu'], ['Ph.D.', 'Professor', 'hongzhe@upenn.edu']]}
                
@@ -17,6 +17,7 @@ professor_dict = {('Susan', 'Ellenberg'): ['Ph.D.', 'Professor', 'sellenbe@upenn
 
 pk = professor_dict.keys()
 
+print("\nQ7")
 for i in list(pk)[0:min(3,len(pk))]:
     print(i)
     print("  ")
@@ -25,6 +26,7 @@ for i in list(pk)[0:min(3,len(pk))]:
 
     
 #Q8
+print("\nQ8")
 srted=sorted(pk, key=lambda x: x[0]+x[1])
 for s in srted:
     print(s)
